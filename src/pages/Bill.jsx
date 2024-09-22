3cb4494d15fde176195f998fbcaf3a9573fd565c
Nguyễn Thành Tâm <141797848+0337742075@users.noreply.github.com>
2024-09-22 15:43:33 +0700

    commit

--- src/pages/Bill.jsx
+++ src/pages/Bill.jsx
@@ -18,7 +18,6 @@ const Bill = () => {
     { field: 'price', headerName: 'Price (VND)', width: 150 },
     { field: 'total', headerName: 'Total (VND)', width: 150 },
   ];
-
   
   return (
     <div style={{fontFamily:'Time new Romance'}} className="dashboard-container">
